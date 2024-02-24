e5e4aec63065f1ee46361a1dd55513617c71c05b
shahinfarahani7 <shahinfarahani7@gmail.com>
2024-02-24 18:03:25 +0330

    responsive

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 
-import Layout from "./layout/Layout";
+
 import Navbar from "./layout/navbar/Navbar";
 import Content from './layout/content/Content'
 import { Routes, Route } from 'react-router-dom'
 import Home from "./pages/home/Home";
 import AddToDo from "./pages/add_to_do/AddToDo";
-import List from "./List";
+
 
 
 function App() {
